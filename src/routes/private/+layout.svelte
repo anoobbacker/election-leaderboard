bd6751789b039eca1c3fb6926c47f0d5ff2ca11c
anoobbacker <anoob.backer@live.com>
2024-05-24 16:01:50 +0530

    Fixing the clicking link issue

--- src/routes/private/+layout.svelte
+++ src/routes/private/+layout.svelte
@@ -24,13 +24,13 @@
   }
 
 </script>
-<!-- TODO: There is bug due to which when url is /private the menu items are not clickable. hence enable menu only for other context. -->
-<nav class="bg-gray-800">
+
+<nav class="bg-gray-800 z-50">
   <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
     <div class="relative flex h-16 items-center justify-between">
       <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
         <!-- Mobile menu button-->
-        <button type="button" class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="{isMenuOpen}" on:click={toggleMenu}>
+        <button type="button" class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="{!isMenuOpen}" on:click={toggleMenu}>
           <span class="absolute -inset-0.5"></span>
           <span class="sr-only">Open main menu</span>
           <!--
@@ -38,20 +38,17 @@
 
             Menu open: "hidden", Menu closed: "block"
           -->
-          {#if isMenuOpen === false}
-          <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+          <svg class="{ (isMenuOpen === false)? 'block' : 'hidden'} h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" >
             <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
           </svg>
-          {:else}
           <!--
             Icon when menu is open.
 
             Menu open: "block", Menu closed: "hidden"
           -->
-          <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+          <svg class="{ (isMenuOpen === true)? 'block' : 'hidden'} h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
           </svg>
-          {/if}
         </button>
       </div>      
       <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
@@ -94,7 +91,7 @@
   </div>
 
   <!-- Mobile menu, show/hide based on menu state. -->
-  <div class="sm:hidden" id="mobile-menu">
+  <div class="sm:hidden" id="mobile-menu" class:!block={isMenuOpen} class:hidden={!isMenuOpen}>
     <div class="space-y-1 px-2 pb-3 pt-2">
       <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
       <a href="/private" class="{currentPath === '/private' ? 'bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium' : 'text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium'}" aria-current={currentPath === '/private' ? 'page' : false}>Home</a>
--- src/routes/private/+page.svelte
+++ src/routes/private/+page.svelte
@@ -9,7 +9,7 @@
 
 </script>
 <div class="relative isolate max-w-7xl px-6 pt-4 lg:px-8">
-  <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
+  <div class="absolute inset-x-0 transform-gpu overflow-hidden blur-3xl" aria-hidden="true">
     <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
   </div>
   <div class="mx-auto max-w-2xl py-32 sm:py-12 lg:py-20">
@@ -23,8 +23,8 @@
       <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Forecast your predictions!</h1>
       <p class="mt-6 text-lg leading-8 text-gray-600">Kerala Election 2024</p>
       <div class="mt-10 flex items-center justify-center gap-x-6">
-        <a href="/private/predict" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Predict</a>
-        <a href="/private/candidate" class="text-sm font-semibold leading-6 text-gray-900">View candidates <span aria-hidden="true">→</span></a>
+        <a href="/private/predict" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 z-40">Predict</a>
+        <a href="/private/candidate" class="text-sm font-semibold leading-6 text-gray-900 z-40">View candidates <span aria-hidden="true">→</span></a>
       </div>
     </div>    
   </div>  
